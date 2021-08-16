08fd4f7ee6e3691c0111c8dc8a5dde2710807714
paukasha <odinpozitivchik@mail.ru>
2021-08-16 20:25:10 +1000

    vue basic2

--- vue-app.js
+++ vue-app.js
@@ -1,8 +1,47 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: 'Счетчик'
+      title: 'Список заметок',
+      placeholderString: 'Введите название заметки',
+      inputValue: '',
+      notes: ['Заметка 1', 'Заметка 2', 'Заметка 100500']
+    }
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value
+    },
+    addNewNote() {
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    inputKeyPress(event) {
+     if (event.key === 'Enter') {
+       this.addNewNote()
+     }
+    },
+    deleteNote(index) {
+      this.notes.splice(index, 1)
+    },
+    toUpperCase(item) {
+      return item.toUpperCase();
+    },
+    doubleCount() {
+      return this.notes.length*2
+    }
+  },
+  computed: {
+    doubleCountComputed() {
+      return this.notes.length*2
+    }
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        this.inputValue=''
+      }
     }
   }
 }
