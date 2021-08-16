b76141d86790ee3b6591f5ac6c4c105a6ae97ecd
paukasha <odinpozitivchik@mail.ru>
2021-08-16 18:32:17 +1000

    vue basic

--- vue-learn/src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h2>Home page</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, at.</p>
-    <router-link to="/todos">Todos</router-link>
-  </div>
-</template>
